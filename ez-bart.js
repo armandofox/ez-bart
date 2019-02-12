90d3b366dc701fe272fb28cbde049ed744bafd85
Armando Fox <fox@cs.berkeley.edu>
2019-02-11 19:35:37 -0800

    initial commit

---
+++ ez-bart.js
@@ -0,0 +1,129 @@
+// Assumes jQuery is loaded.
+
+var EZBart = {
+
+  api_key: "MW9S-E7SL-26DU-VV8V"
+
+  ,setup: function() {
+    EZBart.populate_stations();
+    $('.mode').change(EZBart.toggleButtons);
+    EZBart.populate_time_menu();
+    $('.control').change(EZBart.request);
+  }
+
+  ,toggleButtons: function() {
+    // change from not-checked to checked; called once for each button
+    $('.mode').removeClass('btn-primary').addClass('btn-outline-primary');
+    $(this).addClass('btn-primary');
+  }
+
+  ,request: function() {
+    var params = {
+      "cmd":  $('input:radio[name=cmd]:checked').attr('id'),
+      "orig": $('#depart').val(),
+      "dest":  $('#arrive').val(),
+      "time": $('#time').val(),
+      "b": 2,
+      "a": 2,
+      "json": "y",
+      "key": EZBart.api_key
+    };
+    var base_uri = "http://api.bart.gov/api/";
+    var uri = base_uri + '?' + $.param(params);
+    alert(uri);
+  }
+
+  ,populate_time_menu: function() {
+    var now = new Date();
+    var index = 4 * Math.floor(now.getHours() - 4);
+    for (var i=index; i < EZBart.times.length; i += 1) {
+      var time = EZBart.times[i];
+      $('#time').append(
+        $('<option></option>').attr("value", time).text(time)
+      );
+    }
+  }
+  ,times: [
+    '4:00 am', '4:15 am', '4:30 am', '4:45 am', 
+    '5:00 am', '5:15 am', '5:30 am', '5:45 am', 
+    '6:00 am', '6:15 am', '6:30 am', '6:45 am', 
+    '7:00 am', '7:15 am', '7:30 am', '7:45 am', 
+    '8:00 am', '8:15 am', '8:30 am', '8:45 am', 
+    '9:00 am', '9:15 am', '9:30 am', '9:45 am', 
+    '10:00 am', '10:15 am', '10:30 am', '10:45 am', 
+    '11:00 am', '11:15 am', '11:30 am', '11:45 am', 
+    '12:00 pm', '12:15 pm', '12:30 pm', '12:45 pm', 
+    '1:00 pm', '1:15 pm', '1:30 pm', '1:45 pm', 
+    '2:00 pm', '2:15 pm', '2:30 pm', '2:45 pm', 
+    '3:00 pm', '3:15 pm', '3:30 pm', '3:45 pm', 
+    '4:00 pm', '4:15 pm', '4:30 pm', '4:45 pm', 
+    '5:00 pm', '5:15 pm', '5:30 pm', '5:45 pm', 
+    '6:00 pm', '6:15 pm', '6:30 pm', '6:45 pm', 
+    '7:00 pm', '7:15 pm', '7:30 pm', '7:45 pm', 
+    '8:00 pm', '8:15 pm', '8:30 pm', '8:45 pm', 
+    '9:00 pm', '9:15 pm', '9:30 pm', '9:45 pm', 
+    '10:00 pm', '10:15 pm', '10:30 pm', '10:45 pm', 
+    '11:00 pm', '11:15 pm', '11:30 pm', '11:45 pm'
+  ]
+
+    
+  ,populate_stations: function() {
+    $.each(EZBart.abbrevs, function(val, display_string) {
+      $('#arrive,#depart').append(
+        $('<option></option>').attr("value", val).text(display_string));
+    });
+  }
+  ,abbrevs: {
+    // from https://api.bart.gov/docs/overview/abbrev.aspx
+    "12th": "12th St. Oakland City Center",
+    "16th": "16th St. Mission (SF)",
+    "19th": "19th St. Oakland",
+    "24th": "24th St. Mission (SF)",
+    "ashb": "Ashby (Berkeley)",
+    "antc": "Antioch",
+    "balb": "Balboa Park (SF)",
+    "bayf": "Bay Fair (San Leandro)",
+    "cast": "Castro Valley",
+    "civc": "Civic Center (SF)",
+    "cols": "Coliseum",
+    "colm": "Colma",
+    "conc": "Concord",
+    "daly": "Daly City",
+    "dbrk": "Downtown Berkeley",
+    "dubl": "Dublin/Pleasanton",
+    "deln": "El Cerrito del Norte",
+    "plza": "El Cerrito Plaza",
+    "embr": "Embarcadero (SF)",
+    "frmt": "Fremont",
+    "ftvl": "Fruitvale (Oakland)",
+    "glen": "Glen Park (SF)",
+    "hayw": "Hayward",
+    "lafy": "Lafayette",
+    "lake": "Lake Merritt (Oakland)",
+    "mcar": "MacArthur (Oakland)",
+    "mlbr": "Millbrae",
+    "mont": "Montgomery St. (SF)",
+    "nbrk": "North Berkeley",
+    "ncon": "North Concord/Martinez",
+    "oakl": "Oakland Int'l Airport",
+    "orin": "Orinda",
+    "pitt": "Pittsburg/Bay Point",
+    "pctr": "Pittsburg Center",
+    "phil": "Pleasant Hill",
+    "powl": "Powell St. (SF)",
+    "rich": "Richmond",
+    "rock": "Rockridge (Oakland)",
+    "sbrn": "San Bruno",
+    "sfia": "San Francisco Int'l Airport",
+    "sanl": "San Leandro",
+    "shay": "South Hayward",
+    "ssan": "South San Francisco",
+    "ucty": "Union City",
+    "warm": "Warm Springs/South Fremont",
+    "wcrk": "Walnut Creek",
+    "wdub": "West Dublin",
+    "woak": "West Oakland"
+  }
+}
+
+$(EZBart.setup);
